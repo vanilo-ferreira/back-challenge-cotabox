57b2bffd6b7433700d532287fc21db11ede5042c
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-02-19 18:05:21 -0300

    feat: Adicionar rota login

--- src/rotas.js
+++ src/rotas.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const companies = require('./controllers/companies')
+const companies = require('./controllers/companies');
+const login = require('./controllers/login')
 
 const rotas = express();
 
 rotas.post('/companies', companies.registerCompany);
+
+rotas.post('/login', login.login);
 
 
 module.exports = rotas;
