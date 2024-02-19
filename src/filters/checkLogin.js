d39d9996078c67558ae97c258f0fca79721af9f1
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-02-19 19:14:23 -0300

    feat: Adicionar filtro checkLogin para rotas após os uruários estarem logado

---
+++ src/filters/checkLogin.js
@@ -0,0 +1,35 @@
+const conexao = require('../conexao');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const checkLogin = async (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json("Não autorizado!");
+  }
+
+  try {
+    const token = authorization.replace('Bearer ', '').trim();
+
+    const { id } = jwt.verify(token, process.env.SENHA_HASH);
+
+    const query = 'select * from companies where id = $1';
+
+    const { rows, rowCount } = await conexao.query(query, [id])
+
+    if (rowCount == 0) {
+      return res.status(404).json("Empresa não encontrada!");
+    }
+
+    const { senha, ...company } = rows[0];
+
+    req.company = company;
+
+    next();
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+}
+
+module.exports = checkLogin;
