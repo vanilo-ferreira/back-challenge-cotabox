a47c32ac1098fec6482f46a647d50bd87c92889f
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-02-19 17:18:12 -0300

    feat: Adicionar configurações do arquivo index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const rotas = require('./rotas');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(rotas);
+
+app.listen(8000);
