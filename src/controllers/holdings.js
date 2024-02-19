50a41bf71d1a6873cb6ad8bee96212f84729fdf1
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-02-19 20:36:36 -0300

    feat: Adicionar na função holdings validações referente ao limite da participation

--- src/controllers/holdings.js
+++ src/controllers/holdings.js
@@ -17,14 +17,30 @@ const addParticipation = async (req, res) => {
   }
 
   try {
+    const participationLimit = 100;
+
+    const { rows } = await conexao.query('select sum(participation) from holdings where company_id = $1', [company.id]);
+
+    const total = rows[0].sum;
+
+    if (total + participation >= participationLimit) {
+      if (total >= participationLimit) {
+        return res.status(404).json('Limite de participation alcançado, não é possível adicionar novas participações');
+      }
+      const exceeded = (participation - total);
+      return res.status(404).json(`Excede o limite da participation, o limite é até ${exceeded}%`);
+    }
+
     const query = 'insert into holdings (company_id, first_name, last_name, participation) values ($1, $2, $3, $4)';
     const addedParticipation = await conexao.query(query, [company.id, first_name, last_name, participation]);
 
     if (addedParticipation.rowCount == 0) {
-      return res.status(404).json('A participation não foi cadastrada!');
+      return res.status(404).json('A porcentagem da participation não foi adicionada!');
     }
 
-    return res.status(201).json('A participation foi adicionada com sucesso!');
+    const newLimit = ((participationLimit - total) - participation);
+
+    return res.status(201).json(`A porcentagem da participation foi adicionada com sucesso, restam ${newLimit}%!`);
   } catch (error) {
     return res.status(400).json(error.message);
   }
