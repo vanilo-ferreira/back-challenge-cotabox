bc2353c1e1c0c0e6108e0dfe37e70e19d88b2b5b
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-02-19 19:13:36 -0300

    feat: Adicionar filtro nas rotas

--- src/rotas.js
+++ src/rotas.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const companies = require('./controllers/companies');
-const login = require('./controllers/login')
+const login = require('./controllers/login');
+const checkLogin = require('./filters/checkLogin');
 
 const rotas = express();
 
 rotas.post('/companies', companies.registerCompany);
 
 rotas.post('/login', login.login);
+
+rotas.use(checkLogin);
 
 
 module.exports = rotas;
