1ffce0d59ecca9d96699da9872c270ea8b2e6307
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-02-19 19:37:05 -0300

    feat: Adicionar rota /holdings para adicionar uma participação

--- src/rotas.js
+++ src/rotas.js
@@ -2,6 +2,7 @@ const express = require('express');
 const companies = require('./controllers/companies');
 const login = require('./controllers/login');
 const checkLogin = require('./filters/checkLogin');
+const holdings = require('./controllers/holdings');
 
 const rotas = express();
 
@@ -10,6 +11,8 @@ rotas.post('/companies', companies.registerCompany);
 rotas.post('/login', login.login);
 
 rotas.use(checkLogin);
+
+rotas.post('/holdings', holdings.addParticipation);
 
 
 module.exports = rotas;
