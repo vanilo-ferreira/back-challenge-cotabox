5757093c2658245d93b00fca92a080507231bf8a
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-02-19 18:07:04 -0300

    feat: Adicionar schema do banco de dados

---
+++ schema.sql
@@ -0,0 +1,19 @@
+drop table if exists companies;
+
+drop table if exists participation;
+
+create table companies(
+  id serial primary key,
+  name text not null,
+  email text not null unique,
+  password text not null
+);
+
+create table participation(
+  id serial primary key,
+  firts_name text not null,
+  last_name text not null,
+  participation text not null,
+  company_id integer not null,
+  foreign key (company_id) references companies(id)
+);
