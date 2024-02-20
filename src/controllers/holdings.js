20c299bd4ad83f057270ca61e2f575d76fef76dc
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-02-19 21:47:06 -0300

    feat: Adicionar na função holdings validações de atualização de participation quando o usuário já cadastrado quiser adicionar uma nova participation

--- src/controllers/holdings.js
+++ src/controllers/holdings.js
@@ -12,8 +12,8 @@ const addParticipation = async (req, res) => {
     return res.status(404).json('O campo last_name é obrigatório');
   }
 
-  if (!participation) {
-    return res.status(404).json('O campo participation é obrigatório');
+  if (typeof participation !== "number" || !participation) {
+    return res.status(404).json('O campo participation é obrigatório e deve ser preeenchido com um número válido');
   }
 
   try {
@@ -21,9 +21,9 @@ const addParticipation = async (req, res) => {
 
     const { rows } = await conexao.query('select sum(participation) from holdings where company_id = $1', [company.id]);
 
-    const total = rows[0].sum;
+    const total = parseFloat(rows[0].sum);
 
-    if (total + participation >= participationLimit) {
+    if (total + participation > participationLimit) {
       if (total >= participationLimit) {
         return res.status(404).json('Limite de participation alcançado, não é possível adicionar novas participações');
       }
@@ -31,16 +31,33 @@ const addParticipation = async (req, res) => {
       return res.status(404).json(`Excede o limite da participation, o limite é até ${exceeded}%`);
     }
 
-    const query = 'insert into holdings (company_id, first_name, last_name, participation) values ($1, $2, $3, $4)';
-    const addedParticipation = await conexao.query(query, [company.id, first_name, last_name, participation]);
+    const { rows: colaborador } = await conexao.query(`SELECT * FROM holdings WHERE first_name = $1 and last_name = $2;`, [first_name, last_name]);
 
-    if (addedParticipation.rowCount == 0) {
-      return res.status(404).json('A porcentagem da participation não foi adicionada!');
+    let text = "atualizada";
+
+    if (colaborador.length === 0) {
+      text = "adicionada";
+      const query = 'insert into holdings (company_id, first_name, last_name, participation) values ($1, $2, $3, $4)';
+      const addedParticipation = await conexao.query(query, [company.id, first_name, last_name, participation]);
+
+      if (addedParticipation.rowCount == 0) {
+        return res.status(404).json('A porcentagem da participation não foi adicionada!');
+      }
+    } else {
+      const newParticipation = parseFloat(colaborador[0].participation) + participation;
+
+      const queryAtualizacao = `update holdings set participation = $1 where id = $2`;
+
+      const atualizado = await conexao.query(queryAtualizacao, [newParticipation, colaborador[0].id]);
+
+      if (atualizado.rowCount == 0) {
+        return res.status(404).json('A porcentagem da participation não foi atualizada!');
+      }
     }
 
     const newLimit = ((participationLimit - total) - participation);
 
-    return res.status(201).json(`A porcentagem da participation foi adicionada com sucesso, restam ${newLimit}%!`);
+    return res.status(201).json(`A porcentagem da participation de ${first_name} ${last_name} foi ${text} com sucesso, restam ${newLimit}%!`);
   } catch (error) {
     return res.status(400).json(error.message);
   }
