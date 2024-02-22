7de9549b8295c4632e2570a5e04d69d10fbf48f0
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-02-22 18:59:30 -0300

    feat: Instalar e Importar o pacote cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const rotas = require('./rotas');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(rotas);
 
 app.listen(8000);
