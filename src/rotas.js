aecb80a3aab6efb9a4ef1bcdd32ad75a2d32732f
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-02-22 18:58:51 -0300

    feat: Adicionar as rotas CheckProfile e obterParticipation

--- src/rotas.js
+++ src/rotas.js
@@ -12,7 +12,10 @@ rotas.post('/login', login.login);
 
 rotas.use(checkLogin);
 
+rotas.get('/perfil', companies.CheckProfile);
+
 rotas.post('/holdings', holdings.addParticipation);
+rotas.get('/holdings', holdings.obterParticipation);
 
 
 module.exports = rotas;
